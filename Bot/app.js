40c3bc0ba8d33cc3f1e6f40f1523d7eeb13d03b1
Dylan Fu <dylan.fu@hotmail.co.nz>
2017-11-24 10:35:31 +1300

    Added app ID and password

--- Bot/app.js
+++ Bot/app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "5fb5efb8-8993-4a57-8ed2-ca1eb356a4db",
+    appPassword: "sbWEDNN734!kbdqhYF53:?("
 });
 
 // Listen for messages from users 
